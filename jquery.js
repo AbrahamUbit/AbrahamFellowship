040358e638d47c4782d00d7bfd51cd626ba57813
ABRAHAM UBIT <blessed@ABRAHAM.ABRAHAM>
2019-01-28 14:37:33 -0100

    Me

---
+++ jquery.js
@@ -0,0 +1,21 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    pagination:false,
+    autoplay:0.1,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        700:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+})
